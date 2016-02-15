14443005b64cbbf595e3175735bbf3467063d094
skipsuva <skip.suva@gmail.com>
2016-02-15 17:37:00 -0500

    generate routes

--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('trips', function() {
+    this.route('trip', {path: "/:trip_id"});
+  });
 });
 
 export default Router;
