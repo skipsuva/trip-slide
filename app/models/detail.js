31b5f404092d9192108ce3fbd3f18ff632953baf
skipsuva <skip.suva@gmail.com>
2016-02-15 17:51:37 -0500

    add associations

--- app/models/detail.js
+++ app/models/detail.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   category: DS.attr('string'),
   travelDate: DS.attr('date'),
-  notes: DS.attr('string')
+  notes: DS.attr('string'),
+  trip: DS.belongsTo('trip', { async: true })
 });
--- app/models/trip.js
+++ app/models/trip.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   destination: DS.attr('string'),
-  startDate: DS.attr('date')
+  startDate: DS.attr('date'),
+  details: DS.hasMany('detail', { async: true })
 });
