1c91bf86c4af50cc6117dd369b56fc96df489b1b
skipsuva <skip.suva@gmail.com>
2016-02-15 19:08:52 -0500

    add new trip form and add route logic

--- app/routes/trips/new.js
+++ app/routes/trips/new.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.createRecord('trip');
+  },
+  actions: {
+    addTrip(){
+      let trip = this.modelFor(this.routeName);
+      trip.save();
+      this.transitionTo('trips.trip');
+    }
+  }
 });
